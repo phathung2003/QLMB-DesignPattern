1760681b6206732e22ebfbbc9303b5daea861049
phathung2003 <21dh113722@st.huflit.edu.vn>
2024-03-17 13:03:31 +0700

    Clean code

--- Design_Pattern/Adapter/Adaptee/AdapteeChangeFormat.cs
+++ Design_Pattern/Adapter/Adaptee/AdapteeChangeFormat.cs
@@ -1,10 +1,5 @@
 ﻿using QLMB.Design_Pattern.Prototype.ConcretePrototype;
 using QLMB.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
 namespace QLMB.Design_Pattern.Adapter.Adaptee
 {
     public class AdapteeChangeFormat
