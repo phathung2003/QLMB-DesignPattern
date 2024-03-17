d57145da6a7c2f32855c3c89086a13e846c62356
PaTH13U <lmaoezgame1@gmail.com>
2024-03-17 14:16:40 +0700

    Add Template Method in PropertyController + PropertyRentController

---
+++ Design_Pattern/TemplateMethod/PropertyControllerTemplate.cs
@@ -0,0 +1,18 @@
+﻿using QLMB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QLMB.Design_Pattern.TemplateMethod
+{
+    public class PropertyControllerTemplate : TemplateMethodController
+    {
+        private readonly string ROLE = "MB";
+
+        protected override bool CheckRole()
+        {
+            return ((NhanVien)Session["EmployeeInfo"]).MaChucVu.Trim() == ROLE;
+        }
+    }
+}
---
+++ Design_Pattern/TemplateMethod/TemplateMethodController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLMB.Design_Pattern.TemplateMethod
+{
+    public abstract class TemplateMethodController : Controller
+    {
+        // Template method
+        public bool IsValidRole()
+        {
+            if (Session["EmployeeInfo"] == null)
+                return false;
+
+            if (CheckRole())
+                return true;
+
+            // Xử lý khi không hợp lệ
+            HandleInvalidRole();
+            return false;
+        }
+
+        // Các bước trừu tượng để triển khai trong lớp con
+        protected abstract bool CheckRole();
+        protected virtual void HandleInvalidRole()
+        {
+            // Mặc định: Chuyển hướng đến trang đăng nhập
+            RedirectToAction("Login", "Login");
+        }
+    }
+}
