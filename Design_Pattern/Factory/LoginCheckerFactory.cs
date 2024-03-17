d4f1988a6b7f331f8a2b749a760763bd6b169b07
HuynhNgocBao1412 <117575277+HuynhNgocBao1412@users.noreply.github.com>
2024-03-17 18:21:45 +0700

    add new class and change code login

---
+++ Design_Pattern/Factory/LoginCheckerFactory.cs
@@ -0,0 +1,31 @@
+﻿using QLMB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QLMB.Design_Pattern.Factory
+{
+    public class LoginCheckerFactory
+    {
+        private database db;
+
+        public LoginCheckerFactory(database dbContext)
+        {
+            db = dbContext;
+        }
+
+        public ILoginChecker GetLoginChecker(string role)
+        {
+            if (role.Equals("MB"))
+            {
+                return new RentalLoginChecker(db);
+            }
+            else
+            {
+                return new ManagerLoginChecker(db);
+            }
+
+        }
+    }
+}
---
+++ Design_Pattern/Factory/ManagerLoginChecker.cs
@@ -0,0 +1,75 @@
+﻿using QLMB.Design_Pattern.Strategy.ConcreteStrategy;
+using QLMB.Design_Pattern.Strategy.Context;
+using QLMB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLMB.Design_Pattern.Factory
+{
+    public class ManagerLoginChecker : ILoginChecker
+    {
+        private database db;
+        private ModelStateDictionary modelState;
+        private Controller controller;
+
+        public ManagerLoginChecker(database db)
+        {
+            this.db = db;
+        }
+
+        public ManagerLoginChecker(database dbContext, ModelStateDictionary modelState, Controller controller)
+        {
+            db = dbContext;
+            this.modelState = modelState;
+            this.controller = controller;
+        }
+
+        public bool CheckLogin(string username, string password)
+        {
+            ContextStrategy checkResult;
+
+            //Username
+            checkResult = new ContextStrategy(new ConcreteUsername(modelState, "inputUsername", username));
+            checkResult.GetResult();
+
+            //Password
+            checkResult.strategy = new ConcretePassword(modelState, "inputPassword", password);
+            checkResult.GetResult();
+
+            if (checkResult.noError)
+            {
+                (bool, string, NhanVien) checkLogin = Validation.checkLoginEmployee(username, password);
+
+                //Thấy thông tin => Thông tin đúng
+                if (checkLogin.Item1)
+                {
+                    string[] name = checkLogin.Item3.ThongTinND.HoTen.Split(' ');
+
+                    //Xử lý độ dài tên: Độ dài lớn hơn 1 mới bị cắt 2 tên cuối
+                    if (name.Length == 1)
+                        controller.Session["AccountName"] = name[0];
+                    else
+                        controller.Session["AccountName"] = name[name.Length - 2] + " " + name[name.Length - 1];
+
+                    ThongTinND employeeInfo = db.ThongTinNDs.Where(s => s.CMND == checkLogin.Item3.CMND).FirstOrDefault();
+
+                    controller.Session["EmployeeInfo"] = checkLogin.Item3;
+                    controller.Session["UserInfo"] = employeeInfo;
+
+                    return true;
+                }
+                else
+                {
+                    modelState.AddModelError("Error", checkLogin.Item2);
+                    return false;
+                }
+            }
+
+            //Thông tin sai
+            return false;
+        }
+    }
+}
