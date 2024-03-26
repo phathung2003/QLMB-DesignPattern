0731bef8ffe65ddd07ee1eb251f9e54e293eddbc
phathung2003 <21dh113722@st.huflit.edu.vn>
2024-03-26 07:57:55 +0700

    Fix bug + add feature for Command pattern

--- Controllers/Customer/ForgetPasswordController.cs
+++ Controllers/Customer/ForgetPasswordController.cs
@@ -3,17 +3,10 @@ using System.Web.Mvc;
 using QLMB.Design_Pattern.Command;
 using System.Web;
 using QLMB.Design_Pattern.Command.ConcreteCommand;
-using QLMB.Models.Process;
-using QLMB.Design_Pattern.Strategy.ConcreteStrategy;
-using QLMB.Design_Pattern.Strategy.Context;
-using QLMB.Design_Pattern.Strategy.ConcreteFactory;
-using QLMB.Design_Pattern.Command;
-
 namespace QLMB.Controllers
 {
     public class ForgetPasswordController : Controller
     {
-        private Controller _controller;
         private database database = new database();
 
         //Trang quên mật khẩu
@@ -46,35 +39,7 @@ namespace QLMB.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult rePasswordNguoiThue(NguoiThue nguoiThue, string choice, string rePass)
         {
-            switch (choice)
-            {
-                case "Quay lại": return RedirectToAction("ForgetPassword", "ForgetPassword");
-                default:
-                    if (CheckRePassword(nguoiThue, rePass) == true)
-                    {
-                        nguoiThue.CMND = Session["CMND"].ToString();
-                        nguoiThue.TenDangNhap = Session["TenDangNhap"].ToString();
-                        (bool, string) changePassword = Edit.CustomerPassword(nguoiThue);
-
-                        if (changePassword.Item1)
-                        {
-                            TempData["msg"] = $"<script>alert('{changePassword.Item2}');</script>";
-
-                            //Xoá session
-                            Session.Remove("CMND");
-                            Session.Remove("TenDangNhap");
-                            return RedirectToAction("Login", "Login");
-                        }
-                        ModelState.AddModelError("updateError", "* Lỗi hệ thống - Xin vui lòng thử lại !");
-                    }
-                    return View();
-            }
-        }
-
-
-        //Check mật khẩu mới - [Strategy Pattern]
-        private bool CheckRePassword(NguoiThue nguoiThue, string rePass)
-        {
+            HttpSessionStateBase session = this.Session;
             ModelStateDictionary modelState = this.ModelState;
 
             CommandInvoker invoker = new CommandInvoker();
